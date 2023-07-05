77cab51aa0ee9d8116bc8d7a16cebf84abf967ad
Alexey-Dergai <a.dergai@mail.ru>
2023-07-05 17:24:31 +0300

    Add project files.

---
+++ ChatGPTBot/Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenAI_API.Chat;
+using OpenAI_API;
+
+OpenAIAPI api = new OpenAIAPI(new APIAuthentication("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"));
+Conversation chat = api.Chat.CreateConversation();
+
+while (true)
+{
+    // ввод сообщения пользователя
+    Console.Write("User: ");
+    var content = Console.ReadLine();
+
+    // если введенное сообщение имеет длину меньше 1 символа
+    // то выходим из цикла и завершаем программу
+    if (content is not { Length: > 0 }) break;
+
+    // отправляем запрос
+    chat.AppendUserInput(content);
+
+    // получаем данные ответа
+    var response = await chat.GetResponseFromChatbotAsync();
+
+    if (string.IsNullOrWhiteSpace(response))
+    {
+        Console.WriteLine("Response is empty");
+        continue;
+    }
+
+    Console.WriteLine($"ChatGPT: {response}");
+}
